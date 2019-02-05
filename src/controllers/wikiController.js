66da46afb375c70abda0cb14070329f9de5a869b
Victor <infomagicglove@gmail.com>
2019-02-05 00:15:15 -0500

    wikis1

--- src/controllers/wikiController.js
+++ src/controllers/wikiController.js
@@ -1,92 +1,86 @@
 const wikiQueries = require("../db/queries.wikis");
-const collaboratorQueries = require("../db/queries.collaborators")
+//const Authorizer = require("../policies/application.js");
+//const collaboratorQueries = require("../db/queries.collaborators")
 const userQueries = require("../db/queries.users");
-const markdown = require( "markdown" ).markdown;
+//const markdown = require( "markdown" ).markdown;
 
 module.exports = {
+  index(req, res, next){
+  wikiQueries.getAllWikis((err, wikis) => {
+    if(err){
+      res.redirect(500, "static/index");
+    } else {
+      res.render("wikis/index", {wikis});
+    }
+  })
+},
+  new(req, res, next){
+   const authorized = new Authorizer (req.user).new();
+     if(authorized){
+    res.render("wikis/new");
+   } else{
+    req.flash("notice", "You are not authorized to do that");
+    res.redirect("/wikis");
+   }
 
-    new(req, res, next){
-        res.render("wiki/new", {title: "Create a Wiki"});
-        console.log("new wiki")
-    },
-    index(req, res, next){
-        wikiQueries.getAllWikis(req.user ? req.user.id : null, (err, wikis) => {
+},
 
-            if(err){
-                res.redirect(500, "static/index");
-            } else {
-                res.render("wiki/index", {wikis, title: "Wikis"})
-            }
-        })
-    },
-    create(req, res, next){
+create(req, res, next){
+//const authorized = new Authorizer(req.user).create();
+//if(authorized){
+    let newWiki = {
+    title: req.body.title,
+    body:req.body.body,
+    private: req.body.private,
+      userId: req.user.id
+    };
+    wikiQueries.addWiki(newWiki, (err, wiki) => {
+    if(err){
+      res.redirect(500, "/wikis");
+    } else {
+      res.redirect(303, `/wikis/${wiki.id}`);
+    }
+  });
+  },
+show(req, res, next) {
+    wikiQueries.getWiki(req.params.id, (err, wiki) => {
+       if (err || wiki == null) {
+          res.redirect(404, "/");
+       } else {
+          res.render("wiki/show", {wiki});
+       }
+    });
+  },
 
-        let privacy;
+destroy(req, res, next){
+      wikiQueries.deleteWiki(req, (err, topic) => {
+          if(err){
+              res.redirect(`/wikis/${wiki.id}`)
+           } else {
+            req.flash("notice", "You are not authorized to perform that action");
+   					res.redirect(303, "/wikis");
 
-        if(req.body.privacy === "private"){
-            privacy = true;
-        } else {
-            privacy = false;
-        }
-
-        let newWiki = {
-            title: req.body.title,
-            body: req.body.body,
-            private: privacy,
-            userId: req.user.id
-        };
-        wikiQueries.addWiki(newWiki, (err, wiki) => {
-            if(err){
-                res.redirect(500, "/wikis/new");
-                console.log(err)
-            } else {
-                res.redirect(303, `/wikis/${wiki.id}`);
-            }
-        });
-    },
-    wikiQueries.getWiki(req.params.wikiId, (err, wiki) => {
-            if(err || wiki == null){
-                res.redirect(404, "/")
-            } else {
-                let wikiMDhtml = markdown.toHTML(wiki.body)
-                res.render("wiki/show", {wiki, body: wikiMDhtml, title: "Wiki", isCollaborator});
-            }
-        });
-
-        destroy(req, res, next){
-        wikiQueries.deleteWiki(req, (err, wiki) => {
-            if(err){
-
-                res.redirect(
-                    typeof err === "number" ? err : 500,
-                    `/wikis/${req.params.wikiId}`
-                  );
-            } else {
-                res.redirect(303, "/wikis")
-            }
-        });
-    },
-    edit(req, res, next){
-        wikiQueries.getWiki(req.params.wikiId, (err, wiki) => {
-            if(err || wiki == null){
-                console.log(err)
-                res.redirect(404, "/");
-            } else {
-            }
+           }
+       });
+   },
+   edit(req, res, next) {
+       wikiQueries.getWiki(req.params.id, (err, wiki) => {
+         if(err || wiki == null) {
+             res.redirect(404, "/wikis");
+    } else {
+         req.flash('notice', 'You are not authorized to do that.');
+         res.redirect(`/wikis/${req.params.id}`);
+       }
      })
-     .catch((err) => {
-         console.log(err)
-     })
- },
- update(req, res, next){
-     wikiQueries.updateWiki(req, req.body, (err, wiki) => {
-         if(err || wiki == null){
-             console.log("update err is...")
-             console.log(err)
-           res.redirect(401, `/wikis/${req.params.wikiId}/edit`);
-         } else {
-           res.redirect(`/wikis/${req.params.wikiId}`);
-         }
+   },
+
+update(req, res, next) {
+     wikiQueries.updateWiki(req.params.id, req.body, (err, wiki) => {
+       if(err || wiki == null) {
+           res.redirect(404, `/wikis/${req.params.id}/edit`);
+       } else {
+           res.redirect(`/wikis/${req.params.id}`);
+       }
      });
- },
+ }
 }
---
+++ src/db/queries.wikis.js
@@ -0,0 +1,66 @@
+const Wiki = require ('./models').Wiki;
+const User = require ('./models').User;
+//const Op = Sequelize.Op;
+
+module.exports ={
+
+	getAllWikis(callback){
+		return Wiki.all()
+		.then((wikis) => {
+			callback(null, wikis);
+		});
+	},
+    addWiki(newWiki, callback) {
+        return Wiki.create(newWiki)
+        .then(wiki =>{
+            callback(null,wiki);
+        })
+        .catch((err) =>{
+            callback(err);
+        });
+    },
+    getWiki(id,callback) {
+        return Wiki.findById(id, {
+            include:[
+             {
+                model: User,
+             }
+            ],
+        })
+        .then((wiki) =>{
+            callback(null,wiki);
+        })
+        .catch((err) =>{
+            callback(err);
+        });
+    },
+    updateWiki(id, updatedWiki, callback){
+        return Wiki.findById(id)
+        .then((wiki) => {
+            if(!wiki){
+                return callback("Wiki not found");
+            }
+
+            wiki.update(updatedWiki, {
+                fields: Object.keys(updatedWiki)
+            })
+            .then(() => {
+                callback(null, wiki);
+            })
+            .catch((err) => {
+                callback(err);
+            });
+        });
+    },
+    deleteWiki(id, callback) {
+        return Wiki.destroy({
+           where: {id}
+        })
+        .then((wiki) => {
+           callback(null, wiki);
+        })
+        .catch((err) => {
+           callback(err);
+        });
+     },
+   }
