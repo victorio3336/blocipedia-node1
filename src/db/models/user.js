7dcb59119feec35d24f1a9c97d3e1ca60ab5c60c
Victor <infomagicglove@gmail.com>
2019-02-06 20:54:14 -0500

    addroles

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -14,6 +14,11 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false
     },
+    role: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    }
 
   }, {});
   User.associate = function(models) {
